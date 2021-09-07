cb5be2c85f708d4be620f932e29e671e3404192d
Luiz Pansarini <lpansarini@gmail.com>
2021-09-07 18:13:50 -0300

    fix: return on sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -23,22 +23,23 @@ export function SideBar({ selectedGenreId, handleClickButton }: SideBarProps) {
       setGenres(response.data);
     });
   }, []);
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
 
-  <nav className="sidebar">
-    <span>
-      Watch<p>Me</p>
-    </span>
-
-    <div className="buttons-container">
-      {genres.map((genre) => (
-        <Button
-          key={String(genre.id)}
-          title={genre.title}
-          iconName={genre.name}
-          onClick={() => handleClickButton(genre.id)}
-          selected={selectedGenreId === genre.id}
-        />
-      ))}
-    </div>
-  </nav>;
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
